121671ccddf372d93f2c8ecca97a7e3cc3a90195
Zach Brown <zach@zachbr.io>
2018-10-21 01:50:44 -0400

    Update missing headers

--- src/main/kotlin/io/zachbr/dis4irc/bridge/mutator/mutators/TranslateFormatting.kt
+++ src/main/kotlin/io/zachbr/dis4irc/bridge/mutator/mutators/TranslateFormatting.kt
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Dis4IRC.
+ *
+ * Dis4IRC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dis4IRC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dis4IRC.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.zachbr.dis4irc.bridge.mutator.mutators
 
 import io.zachbr.dis4irc.bridge.message.Channel
