4dc4a9a7368afcc90c961a30375921b99c574b1e
Zach Brown <zach@zachbr.io>
2018-11-04 04:41:12 -0500

    Add IRC CTCP ACTION support

--- src/main/kotlin/io/zachbr/dis4irc/bridge/pier/irc/IrcMessageListener.kt
+++ src/main/kotlin/io/zachbr/dis4irc/bridge/pier/irc/IrcMessageListener.kt
@@ -19,7 +19,11 @@ package io.zachbr.dis4irc.bridge.pier.irc
 
 import io.zachbr.dis4irc.bridge.message.Message
 import net.engio.mbassy.listener.Handler
+import org.kitteh.irc.client.library.event.channel.ChannelCtcpEvent
 import org.kitteh.irc.client.library.event.channel.ChannelMessageEvent
+import org.kitteh.irc.client.library.util.Format
+
+private const val CTCP_ACTION = "ACTION"
 
 /**
  * Responsible for listening to incoming IRC messages and filtering garbage
@@ -40,6 +44,31 @@ class IrcMessageListener(private val pier: IrcPier) {
         val sender = event.actor.asBridgeSender()
         val source = event.channel.asBridgeSource()
         val message = Message(event.message, sender, source, receiveTimestamp)
+        pier.sendToBridge(message)
+    }
+
+    @Handler
+    fun onCtcp(event: ChannelCtcpEvent) {
+        // ignore messages sent by this bot
+        if (event.actor.nick == pier.getBotNick()) {
+            return
+        }
+
+        val receiveTimestamp = System.nanoTime()
+        logger.debug("IRC CTCP ${event.channel.name} ${event.actor.nick}: ${event.message}")
+
+
+        // if it's not an action we probably don't care
+        if (!event.message.startsWith(CTCP_ACTION)) {
+            return
+        }
+
+        // add italic code and strip off the ctcp type info
+        val messageText = "${Format.ITALIC}${event.message.substring(CTCP_ACTION.length + 1)}"
+
+        val sender = event.actor.asBridgeSender()
+        val source = event.channel.asBridgeSource()
+        val message = Message(messageText, sender, source, receiveTimestamp)
         pier.sendToBridge(message)
     }
 }
