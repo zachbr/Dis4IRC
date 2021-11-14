9af4b866019edd8d560e04dca440cb055cbc7d73
Zach Brown <zach@zachbr.io>
2021-11-14 14:46:36 -0600

    Update project dependencies
    JDA, discord-webhooks, configurate-hocon, slf4j, log4j-core,
    log4j-slf4j-impl, junit-jupiter-api, junit-jupiter-engine
    
    commonmark to follow (probably - pending testing).

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,23 +22,23 @@ repositories {
 
 dependencies {
     implementation("org.kitteh.irc:client-lib:8.0.0")
-    implementation("club.minnced:discord-webhooks:0.5.7")
-    implementation("net.dv8tion:JDA:4.3.0_297") {
+    implementation("club.minnced:discord-webhooks:0.7.2")
+    implementation("net.dv8tion:JDA:4.3.0_346") {
         exclude(module = "opus-java")
     }
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.json:json:20180813")
-    implementation("org.spongepowered:configurate-hocon:4.1.1")
+    implementation("org.spongepowered:configurate-hocon:4.1.2")
     implementation("com.atlassian.commonmark:commonmark:0.15.2")
     implementation("com.atlassian.commonmark:commonmark-ext-gfm-strikethrough:0.15.2")
 
-    implementation("org.slf4j:slf4j-api:1.7.30")
-    implementation("org.apache.logging.log4j:log4j-core:2.13.2")
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.13.2")
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
+    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks {
