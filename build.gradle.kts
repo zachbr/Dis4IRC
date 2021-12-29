1707e471eaeeea22b6f675de6027c8cd2f0aaaf5
Zach Brown <zach@zachbr.io>
2021-12-28 20:16:33 -0600

    Update Log4j to 2.17.1 - Unlikely to affect you
    Another exploit was found in Log4j, however this one (and really all of
    them other than the original CVE-2021-44228 that was fixed in Log4j
    2.15.0 and Dis4IRC 1.4.0) is relatively minor.
    
    "... vulnerable to a remote code execution (RCE) attack where an
    attacker **with permission to modify the logging configuration file** can
    construct a malicious configuration ..." (emphasis mine).
    
    If an attacker has permission to modify your logging configuration, you
    are almost certainly already compromised.

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,8 @@ dependencies {
     implementation("com.atlassian.commonmark:commonmark-ext-gfm-strikethrough:0.15.2")
 
     implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.0")
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.17.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
+    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
@@ -106,6 +106,6 @@ fun getGitHash(): String {
 
 dependencyManagement {
     imports {
-        mavenBom("org.apache.logging.log4j:log4j-bom:2.17.0")
+        mavenBom("org.apache.logging.log4j:log4j-bom:2.17.1")
     }
 }
