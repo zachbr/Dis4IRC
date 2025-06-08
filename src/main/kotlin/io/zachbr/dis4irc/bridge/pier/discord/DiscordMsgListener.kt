zachbr/Dis4IRC

commit 1c74833dc0b06874cae77a66c28ff383daaf3bbc

Refactor bridge message types into disparate classes
Refactor our message type classes so that only the relevant data is
exposed on each type of message. E.g. a message that originates from IRC
no longer has access to an attachments field, as obviously IRC messages
do not have attachments.

This ends up being quite a large refactor and affects a substantial
amount of the codebase. Testing was done to ensure basic functionality
however it would not surprise me if a new bug or two was introduced by
this change. However, it's also at least possible that this clarity has
fixed an old issue or two (though I can't think of one off the top of
my head).

src/main/kotlin/io/zachbr/dis4irc/bridge/pier/discord/DiscordMsgListener.kt
@@ -25,7 +25,7 @@ class DiscordMsgListener(private val pier: DiscordPier) : ListenerAdapter() {
         }
 
         // don't bridge itself
-        val source = event.channel.asTextChannel().asBridgeSource() // if we bridge DMs in the future, this needs to change
+        val source = event.channel.asTextChannel().asPlatformSource() // if we bridge DMs in the future, this needs to change
         if (pier.isThisBot(source, event.author.idLong)) {
             return
         }
@@ -43,6 +43,6 @@ class DiscordMsgListener(private val pier: DiscordPier) : ListenerAdapter() {
         val receiveTimestamp = System.nanoTime()
         logger.debug("DISCORD MSG ${event.channel.name} ${event.author.name}: ${event.message.contentStripped}")
 
-        pier.sendToBridge(message.toBridgeMsg(logger, receiveTimestamp))
+        pier.sendToBridge(message.toPlatformMessage(logger, receiveTimestamp))
     }
 }

src/main/kotlin/io/zachbr/dis4irc/bridge/pier/discord/Extensions.kt
@@ -9,10 +9,9 @@
 package io.zachbr.dis4irc.bridge.pier.discord
 
 import io.zachbr.dis4irc.bridge.message.Embed
-import io.zachbr.dis4irc.bridge.message.MessageSnapshot
-import io.zachbr.dis4irc.bridge.message.PlatformType
-import io.zachbr.dis4irc.bridge.message.Sender
-import io.zachbr.dis4irc.bridge.message.Source
+import io.zachbr.dis4irc.bridge.message.DiscordMessageSnapshot
+import io.zachbr.dis4irc.bridge.message.DiscordSender
+import io.zachbr.dis4irc.bridge.message.DiscordSource
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.entities.channel.ChannelType
@@ -20,14 +19,15 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel
 import net.dv8tion.jda.api.entities.sticker.Sticker
 import org.slf4j.Logger
 import java.net.URLEncoder
+import java.time.Instant
 
 const val DISCORD_STICKER_MEDIA_URL = "https://media.discordapp.net/stickers/%%ID%%.%%FILETYPE%%?size=256"
 const val LOTTIE_PLAYER_BASE_URL = "https://lottie.zachbr.io"
 const val CDN_DISCORDAPP_STICKERS_URL_LENGTH = "https://cdn.discordapp.com/stickers/".length
 
-fun TextChannel.asBridgeSource(): Source = Source(this.name, this.idLong, PlatformType.DISCORD)
+fun TextChannel.asPlatformSource(): DiscordSource = DiscordSource(this.name, this.idLong)
 
-fun Message.toBridgeMsg(logger: Logger, receiveTimestamp: Long = System.nanoTime(), shouldResolveReference: Boolean = true) : io.zachbr.dis4irc.bridge.message.Message {
+fun Message.toPlatformMessage(logger: Logger, receiveTimestamp: Long = System.nanoTime(), shouldResolveReference: Boolean = true) : io.zachbr.dis4irc.bridge.message.DiscordMessage {
     // We need to get the guild member in order to grab their display name
     val guildMember = this.guild.getMember(this.author)
     if (guildMember == null && !this.author.isBot) {
@@ -72,14 +72,14 @@ fun Message.toBridgeMsg(logger: Logger, receiveTimestamp: Long = System.nanoTime
     val parsedEmbeds = parseEmbeds(embeds)
 
     // discord replies
-    var bridgeMsgRef: io.zachbr.dis4irc.bridge.message.Message? = null
+    var platformMsgRef: io.zachbr.dis4irc.bridge.message.DiscordMessage? = null
     val discordMsgRef = this.referencedMessage
     if (shouldResolveReference && discordMsgRef != null) {
-        bridgeMsgRef = discordMsgRef.toBridgeMsg(logger, receiveTimestamp, shouldResolveReference = false) // do not endlessly resolve references
+        platformMsgRef = discordMsgRef.toPlatformMessage(logger, receiveTimestamp, shouldResolveReference = false) // do not endlessly resolve references
     }
 
     // forwards
-    val snapshots = ArrayList<MessageSnapshot>()
+    val snapshots = ArrayList<DiscordMessageSnapshot>()
     for (snapshot in this.messageSnapshots) {
         val snapshotAttachmentUrls = parseAttachments(snapshot.attachments)
         for (customEmoji in this.mentions.customEmojis) {
@@ -112,7 +112,7 @@ fun Message.toBridgeMsg(logger: Logger, receiveTimestamp: Long = System.nanoTime
             }
         }
 
-        snapshots.add(MessageSnapshot(
+        snapshots.add(DiscordMessageSnapshot(
             snapshotText,
             snapshotAttachmentUrls,
             snapshotEmbeds
@@ -120,20 +120,20 @@ fun Message.toBridgeMsg(logger: Logger, receiveTimestamp: Long = System.nanoTime
     }
 
     val displayName = guildMember?.effectiveName ?: this.author.name // webhooks won't have an effective name
-    val sender = Sender(displayName, this.author.idLong, null)
+    val sender = DiscordSender(displayName, this.author.idLong)
     if (this.channelType != ChannelType.TEXT) {
         logger.debug("Encountered unsupported channel type: {}", channelType) // TODO: probably a nicer way to handle this (FIXME: Support other types?)
     }
-    val channel = this.channel.asTextChannel().asBridgeSource()
-    return io.zachbr.dis4irc.bridge.message.Message(
+    val channel = this.channel.asTextChannel().asPlatformSource()
+    return io.zachbr.dis4irc.bridge.message.DiscordMessage(
         messageText,
         sender,
         channel,
-        receiveTimestamp,
+        Instant.now(),
         attachmentUrls,
-        bridgeMsgRef,
         parsedEmbeds,
-        snapshots
+        snapshots,
+        platformMsgRef
     )
 }
 

src/main/kotlin/io/zachbr/dis4irc/bridge/pier/irc/Extensions.kt
@@ -8,13 +8,12 @@
 
 package io.zachbr.dis4irc.bridge.pier.irc
 
-import io.zachbr.dis4irc.bridge.message.PlatformType
-import io.zachbr.dis4irc.bridge.message.Sender
-import io.zachbr.dis4irc.bridge.message.Source
+import io.zachbr.dis4irc.bridge.message.IrcSender
+import io.zachbr.dis4irc.bridge.message.IrcSource
 import org.kitteh.irc.client.library.element.Channel
 import org.kitteh.irc.client.library.element.User
 import java.util.Optional
 
-fun Channel.asBridgeSource(): Source = Source(this.name, null, PlatformType.IRC)
-fun User.asBridgeSender(): Sender = Sender(this.nick, null, this.account.toNullable())
+fun Channel.asBridgeSource(): IrcSource = IrcSource(this.name)
+fun User.asBridgeSender(): IrcSender = IrcSender(this.nick, this.account.toNullable())
 fun <T : Any> Optional<T>.toNullable(): T? = this.orElse(null)

src/main/kotlin/io/zachbr/dis4irc/bridge/Bridge.kt
@@ -11,16 +11,22 @@ package io.zachbr.dis4irc.bridge
 import io.zachbr.dis4irc.Dis4IRC
 import io.zachbr.dis4irc.bridge.command.COMMAND_PREFIX
 import io.zachbr.dis4irc.bridge.command.CommandManager
-import io.zachbr.dis4irc.bridge.message.Message
+import io.zachbr.dis4irc.bridge.message.BridgeMessage
+import io.zachbr.dis4irc.bridge.message.CommandMessage
+import io.zachbr.dis4irc.bridge.message.DiscordMessage
+import io.zachbr.dis4irc.bridge.message.DiscordSource
+import io.zachbr.dis4irc.bridge.message.IrcMessage
+import io.zachbr.dis4irc.bridge.message.IrcSource
 import io.zachbr.dis4irc.bridge.message.PlatformType
 import io.zachbr.dis4irc.bridge.mutator.MutatorManager
 import io.zachbr.dis4irc.bridge.pier.discord.DiscordPier
 import io.zachbr.dis4irc.bridge.pier.irc.IrcPier
 import org.json.JSONObject
 import org.slf4j.LoggerFactory
+import java.time.Instant
 
 /**
- * Responsible for the connection between Discord and IRC, including message processing hand offs
+ * Responsible for the connection between Discord and IRC, including message processing hand-offs
  */
 class Bridge(private val main: Dis4IRC, internal val config: BridgeConfiguration) {
     internal val logger = LoggerFactory.getLogger(config.bridgeName) ?: throw IllegalStateException("Could not init logger")
@@ -55,29 +61,50 @@ class Bridge(private val main: Dis4IRC, internal val config: BridgeConfiguration
     /**
      * Bridges communication between the two piers
      */
-    internal fun submitMessage(messageIn: Message) {
-        val bridgeTarget: String? = channelMappings.getMappingFor(messageIn.source)
+    internal fun submitMessage(bMessage: BridgeMessage) {
+        val mutatedMessage = mutatorManager.applyMutators(bMessage) ?: return
+        val sourceMessage = mutatedMessage.message
 
-        if (bridgeTarget == null) {
-            logger.debug("Discarding message with no bridge target from: {}", messageIn.source)
+        val bridgeTarget = channelMappings.getMappingFor(sourceMessage.source) ?: run {
+            logger.debug("Discarding message with no bridge target from: {}", sourceMessage.source)
             return
         }
 
-        // mutate message contents
-        val mutatedMessage = mutatorManager.applyMutators(messageIn) ?: return
+        // we only send across the bridge (to the relevant mapping) or back to the same source currently
+        val ircSendTarget: String
+        val discordSendTarget: String
+        when (sourceMessage) {
+            is IrcMessage -> {
+                ircSendTarget = sourceMessage.source.channelName
+                discordSendTarget = bridgeTarget
+            }
+            is DiscordMessage -> {
+                ircSendTarget = bridgeTarget
+                discordSendTarget = sourceMessage.source.channelId.toString()
+            }
+            is CommandMessage -> {
+                when (sourceMessage.source) {
+                    is IrcSource -> {
+                        ircSendTarget = sourceMessage.source.channelName
+                        discordSendTarget = bridgeTarget
+                    }
+                    is DiscordSource -> {
+                        ircSendTarget = bridgeTarget
+                        discordSendTarget = sourceMessage.source.channelId.toString()
+                    }
+                }
+            }
+        }
 
         if (mutatedMessage.shouldSendTo(PlatformType.IRC)) {
-            val target: String = if (mutatedMessage.source.type == PlatformType.IRC) mutatedMessage.source.channelName else bridgeTarget
-            ircConn.sendMessage(target, mutatedMessage)
+            ircConn.sendMessage(ircSendTarget, mutatedMessage)
         }
 
         if (mutatedMessage.shouldSendTo(PlatformType.DISCORD)) {
-            val target = if (mutatedMessage.source.type == PlatformType.DISCORD) mutatedMessage.source.discordId.toString() else bridgeTarget
-            discordConn.sendMessage(target, mutatedMessage)
+            discordConn.sendMessage(discordSendTarget, mutatedMessage)
         }
 
-        // command handling
-        if (mutatedMessage.contents.startsWith(COMMAND_PREFIX) && !mutatedMessage.originatesFromBridgeItself()) {
+        if (mutatedMessage.message.contents.startsWith(COMMAND_PREFIX) && !mutatedMessage.originatesFromBridgeItself()) {
             commandManager.processCommandMessage(mutatedMessage)
         }
     }
@@ -109,7 +136,7 @@ class Bridge(private val main: Dis4IRC, internal val config: BridgeConfiguration
     /**
      * Adds a message's handling time to the bridge's collection for monitoring purposes
      */
-    fun updateStatistics(message: Message, timestampOut: Long) {
-        statsManager.processMessage(message, timestampOut)
+    fun updateStatistics(message: BridgeMessage, sendInstant: Instant) {
+        statsManager.processMessage(message, sendInstant)
     }
 }

src/main/kotlin/io/zachbr/dis4irc/bridge/ChannelMappingManager.kt
@@ -8,8 +8,9 @@
 
 package io.zachbr.dis4irc.bridge
 
-import io.zachbr.dis4irc.bridge.message.PlatformType
-import io.zachbr.dis4irc.bridge.message.Source
+import io.zachbr.dis4irc.bridge.message.DiscordSource
+import io.zachbr.dis4irc.bridge.message.IrcSource
+import io.zachbr.dis4irc.bridge.message.PlatformSource
 import java.util.Locale
 
 /**
@@ -31,10 +32,10 @@ class ChannelMappingManager(conf: BridgeConfiguration) {
     /**
      * Gets the opposite channel mapping for the given channel
      */
-    internal fun getMappingFor(source: Source): String? {
-        return when (source.type) {
-            PlatformType.IRC -> ircMappingByName(source.channelName)
-            PlatformType.DISCORD -> discordMappingByName(source.discordId.toString()) ?: discordMappingByName(source.channelName)
+    internal fun getMappingFor(source: PlatformSource): String? {
+        return when (source) {
+            is IrcSource -> ircMappingByName(source.channelName)
+            is DiscordSource -> discordMappingByName(source.channelId.toString()) ?: discordMappingByName(source.channelName)
         }
     }
 

src/main/kotlin/io/zachbr/dis4irc/bridge/StatisticsManager.kt
@@ -8,11 +8,15 @@
 
 package io.zachbr.dis4irc.bridge
 
-import io.zachbr.dis4irc.bridge.message.Message
-import io.zachbr.dis4irc.bridge.message.PlatformType
+import io.zachbr.dis4irc.bridge.message.BridgeMessage
+import io.zachbr.dis4irc.bridge.message.CommandMessage
+import io.zachbr.dis4irc.bridge.message.DiscordMessage
+import io.zachbr.dis4irc.bridge.message.IrcMessage
 import io.zachbr.dis4irc.util.WrappingLongArray
 import org.json.JSONObject
 import java.math.BigInteger
+import java.time.Duration
+import java.time.Instant
 import java.util.concurrent.TimeUnit
 
 /**
@@ -26,20 +30,21 @@ class StatisticsManager(private val bridge: Bridge) {
     /**
      * Processes a message, adding it to whatever statistic counters it needs
      */
-    fun processMessage(message: Message, nanoTimestamp: Long) {
+    fun processMessage(bMessage: BridgeMessage, sentInstant: Instant) {
         // don't count bot, command, etc messages
-        if (message.originatesFromBridgeItself()) {
+        if (bMessage.originatesFromBridgeItself()) {
             return
         }
 
-        when (message.source.type) {
-            PlatformType.DISCORD -> totalFromDiscord++
-            PlatformType.IRC -> totalFromIrc++
+        val message = bMessage.message
+        when (message) {
+            is DiscordMessage -> totalFromDiscord++
+            is IrcMessage -> totalFromIrc++
+            is CommandMessage -> return // command messages originate on bridge, don't need to count
         }
 
-        val difference = nanoTimestamp - message.timestamp
+        val difference = Duration.between(message.timestamp, sentInstant).toNanos()
         messageTimings.add(difference)
-
         bridge.logger.debug("Message from ${message.source.channelName} ${message.sender.displayName} took ${TimeUnit.NANOSECONDS.toMillis(difference)}ms to handle")
     }
 

src/main/kotlin/io/zachbr/dis4irc/bridge/command/CommandManager.kt
@@ -12,10 +12,12 @@ import io.zachbr.dis4irc.bridge.Bridge
 import io.zachbr.dis4irc.bridge.command.api.Executor
 import io.zachbr.dis4irc.bridge.command.executors.PinnedMessagesCommand
 import io.zachbr.dis4irc.bridge.command.executors.StatsCommand
-import io.zachbr.dis4irc.bridge.message.BOT_SENDER
+import io.zachbr.dis4irc.bridge.message.BridgeMessage
+import io.zachbr.dis4irc.bridge.message.BridgeSender
+import io.zachbr.dis4irc.bridge.message.CommandMessage
 import io.zachbr.dis4irc.bridge.message.Destination
-import io.zachbr.dis4irc.bridge.message.Message
 import org.spongepowered.configurate.CommentedConfigurationNode
+import java.time.Instant
 
 const val COMMAND_PREFIX: String = "!"
 
@@ -61,22 +63,19 @@ class CommandManager(private val bridge: Bridge, config: CommentedConfigurationN
     /**
      * Process a command message, passing it off to the registered executor
      */
-    fun processCommandMessage(command: Message) {
-        val split = command.contents.split(" ")
+    fun processCommandMessage(command: BridgeMessage) {
+        val platMessage = command.message
+        val split = platMessage.contents.split(" ")
         val trigger = split[0].substring(COMMAND_PREFIX.length, split[0].length) // strip off command prefix
         val executor = getExecutorFor(trigger) ?: return
 
         logger.debug("Passing command to executor: {}", executor)
-
-        command.destination = Destination.BOTH // we want results to go to both sides by default
-        val result = executor.onCommand(command)
-
+        val result = executor.onCommand(platMessage)
         if (result != null) {
-            command.contents = result
-            command.sender = BOT_SENDER
-
             // submit as new message
-            bridge.submitMessage(command)
+            val resultMessage = BridgeMessage(CommandMessage(result, BridgeSender, platMessage.source, Instant.now()))
+            resultMessage.destination = Destination.BOTH // theoretically the source message was bridged, so the result should go to both places
+            bridge.submitMessage(resultMessage)
         }
     }
 }

src/main/kotlin/io/zachbr/dis4irc/bridge/command/api/Executor.kt
@@ -8,7 +8,7 @@
 
 package io.zachbr.dis4irc.bridge.command.api
 
-import io.zachbr.dis4irc.bridge.message.Message
+import io.zachbr.dis4irc.bridge.message.PlatformMessage
 
 interface Executor {
     /**
@@ -16,5 +16,5 @@ interface Executor {
      *
      * @return your desired output or null if you desire no output
      */
-    fun onCommand(command: Message): String?
+    fun onCommand(command: PlatformMessage): String?
 }

commit ba48677c54d9bf97b263c0b66d984c37abbe2873

Fix receive time tracking of Discord messages
Broken in 1c74833dc0b06874cae77a66c28ff383daaf3bbc

src/main/kotlin/io/zachbr/dis4irc/bridge/pier/discord/DiscordMsgListener.kt
@@ -11,6 +11,7 @@ package io.zachbr.dis4irc.bridge.pier.discord
 import net.dv8tion.jda.api.entities.channel.ChannelType
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.time.Instant
 
 /**
  * Responsible for listening to incoming discord messages and filtering garbage
@@ -40,9 +41,8 @@ class DiscordMsgListener(private val pier: DiscordPier) : ListenerAdapter() {
             return
         }
 
-        val receiveTimestamp = System.nanoTime()
+        val receiveInstant = Instant.now()
         logger.debug("DISCORD MSG ${event.channel.name} ${event.author.name}: ${event.message.contentStripped}")
-
-        pier.sendToBridge(message.toPlatformMessage(logger, receiveTimestamp))
+        pier.sendToBridge(message.toPlatformMessage(logger, receiveInstant))
     }
 }

src/main/kotlin/io/zachbr/dis4irc/bridge/pier/discord/Extensions.kt
@@ -27,7 +27,7 @@ const val CDN_DISCORDAPP_STICKERS_URL_LENGTH = "https://cdn.discordapp.com/stick
 
 fun TextChannel.asPlatformSource(): DiscordSource = DiscordSource(this.name, this.idLong)
 
-fun Message.toPlatformMessage(logger: Logger, receiveTimestamp: Long = System.nanoTime(), shouldResolveReference: Boolean = true) : io.zachbr.dis4irc.bridge.message.DiscordMessage {
+fun Message.toPlatformMessage(logger: Logger, receiveInstant: Instant, shouldResolveReference: Boolean = true) : io.zachbr.dis4irc.bridge.message.DiscordMessage {
     // We need to get the guild member in order to grab their display name
     val guildMember = this.guild.getMember(this.author)
     if (guildMember == null && !this.author.isBot) {
@@ -75,7 +75,7 @@ fun Message.toPlatformMessage(logger: Logger, receiveTimestamp: Long = System.na
     var platformMsgRef: io.zachbr.dis4irc.bridge.message.DiscordMessage? = null
     val discordMsgRef = this.referencedMessage
     if (shouldResolveReference && discordMsgRef != null) {
-        platformMsgRef = discordMsgRef.toPlatformMessage(logger, receiveTimestamp, shouldResolveReference = false) // do not endlessly resolve references
+        platformMsgRef = discordMsgRef.toPlatformMessage(logger, receiveInstant, shouldResolveReference = false) // do not endlessly resolve references
     }
 
     // forwards
@@ -129,7 +129,7 @@ fun Message.toPlatformMessage(logger: Logger, receiveTimestamp: Long = System.na
         messageText,
         sender,
         channel,
-        Instant.now(),
+        receiveInstant,
         attachmentUrls,
         parsedEmbeds,
         snapshots,