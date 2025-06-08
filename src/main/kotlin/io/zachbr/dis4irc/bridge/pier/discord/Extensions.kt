zachbr/Dis4IRC

commit ba48677c54d9bf97b263c0b66d984c37abbe2873

Fix receive time tracking of Discord messages
Broken in 1c74833dc0b06874cae77a66c28ff383daaf3bbc

src/main/kotlin/io/zachbr/dis4irc/bridge/pier/discord/Extensions.kt
@@ -27,7 +27,7 @@ const val CDN_DISCORDAPP_STICKERS_URL_LENGTH = "https://cdn.discordapp.com/stick
 
 fun TextChannel.asPlatformSource(): DiscordSource = DiscordSource(this.name, this.idLong)
 
-fun Message.toPlatformMessage(logger: Logger, receiveTimestamp: Long = System.nanoTime(), shouldResolveReference: Boolean = true) : io.zachbr.dis4irc.bridge.message.DiscordMessage {
+fun Message.toPlatformMessage(logger: Logger, receiveInstant: Instant, shouldResolveReference: Boolean = true) : io.zachbr.dis4irc.bridge.message.DiscordMessage {
     // We need to get the guild member in order to grab their display name
     val guildMember = this.guild.getMember(this.author)
     if (guildMember == null && !this.author.isBot) {
@@ -75,7 +75,7 @@ fun Message.toPlatformMessage(logger: Logger, receiveTimestamp: Long = System.na
     var platformMsgRef: io.zachbr.dis4irc.bridge.message.DiscordMessage? = null
     val discordMsgRef = this.referencedMessage
     if (shouldResolveReference && discordMsgRef != null) {
-        platformMsgRef = discordMsgRef.toPlatformMessage(logger, receiveTimestamp, shouldResolveReference = false) // do not endlessly resolve references
+        platformMsgRef = discordMsgRef.toPlatformMessage(logger, receiveInstant, shouldResolveReference = false) // do not endlessly resolve references
     }
 
     // forwards
@@ -129,7 +129,7 @@ fun Message.toPlatformMessage(logger: Logger, receiveTimestamp: Long = System.na
         messageText,
         sender,
         channel,
-        Instant.now(),
+        receiveInstant,
         attachmentUrls,
         parsedEmbeds,
         snapshots,

src/main/kotlin/io/zachbr/dis4irc/bridge/pier/discord/DiscordMsgListener.kt
@@ -11,6 +11,7 @@ package io.zachbr.dis4irc.bridge.pier.discord
 import net.dv8tion.jda.api.entities.channel.ChannelType
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.time.Instant
 
 /**
  * Responsible for listening to incoming discord messages and filtering garbage
@@ -40,9 +41,8 @@ class DiscordMsgListener(private val pier: DiscordPier) : ListenerAdapter() {
             return
         }
 
-        val receiveTimestamp = System.nanoTime()
+        val receiveInstant = Instant.now()
         logger.debug("DISCORD MSG ${event.channel.name} ${event.author.name}: ${event.message.contentStripped}")
-
-        pier.sendToBridge(message.toPlatformMessage(logger, receiveTimestamp))
+        pier.sendToBridge(message.toPlatformMessage(logger, receiveInstant))
     }
 }

commit 8bf5265d21576f75f10cbd3da13c191688cde1dc

Re-add default value for Instant in Discord msg handler
We use this default value when we retrieve past messages like pinned
messages where we don't want to use the actual "sent" time as discord
received it but instead want to know when our app received it.

This fixes the build that I broke in
ba48677c54d9bf97b263c0b66d984c37abbe2873 while trying to fix a
regression added in 1c74833dc0b06874cae77a66c28ff383daaf3bbc.

src/main/kotlin/io/zachbr/dis4irc/bridge/pier/discord/Extensions.kt
@@ -27,7 +27,7 @@ const val CDN_DISCORDAPP_STICKERS_URL_LENGTH = "https://cdn.discordapp.com/stick
 
 fun TextChannel.asPlatformSource(): DiscordSource = DiscordSource(this.name, this.idLong)
 
-fun Message.toPlatformMessage(logger: Logger, receiveInstant: Instant, shouldResolveReference: Boolean = true) : io.zachbr.dis4irc.bridge.message.DiscordMessage {
+fun Message.toPlatformMessage(logger: Logger, receiveInstant: Instant = Instant.now(), shouldResolveReference: Boolean = true) : io.zachbr.dis4irc.bridge.message.DiscordMessage {
     // We need to get the guild member in order to grab their display name
     val guildMember = this.guild.getMember(this.author)
     if (guildMember == null && !this.author.isBot) {