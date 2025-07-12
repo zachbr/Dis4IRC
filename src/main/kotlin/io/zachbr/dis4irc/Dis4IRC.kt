f2c73f068c01fa138148dbf41a50c73b039a0eb3
Zach Brown <zach@zachbr.io>
2025-07-11 20:03:07 -0500

    Add warning message for users not on Java 21
    Due to a variety of factors, including library dependencies looking to
    update their own minimum version of Java (e.g. outside our control), we
    should warn users that future versions will likely require a newer
    version of Java.
    
    There is no set timeline for requiring Java 21 on Dis4IRC's part.
    If/when we need to require the newer version, we will defer the change
    until the next feature release. E.g. 1.X.0, where X indicates the
    feature version.
    
    Most recent Linux distributions and *nix package managers already offer
    Java 21 (and some default to it) so this is not expected to be a major
    problem for most - however, we should be nice and try and warn those who
    are not on it.

--- src/main/kotlin/io/zachbr/dis4irc/Dis4IRC.kt
+++ src/main/kotlin/io/zachbr/dis4irc/Dis4IRC.kt
@@ -50,6 +50,22 @@ class Dis4IRC(args: Array<String>) {
         logger.info("Source available at ${Versioning.sourceRepo}")
         logger.info("Available under the MIT License")
 
+        // future versions will require a newer version of Java (21)
+        val javaVer = Runtime.version().feature()
+        if (javaVer < 21) {
+            logger.info("")
+            logger.info("======================================================")
+            logger.info("Future versions of Dis4IRC will require Java 21 or newer.")
+            logger.info("You appear to be running Java $javaVer.")
+            logger.info("")
+            logger.info("On Linux, your distribution likely already offers newer packages. If your")
+            logger.info("distribution does not offer newer packages or you are not on Linux, you can use")
+            logger.info("a (free) release package from a vendor such as:")
+            logger.info("    Eclipse Temurin, Amazon Corretto, Microsoft OpenJDK, BellSoft Liberica")
+            logger.info("======================================================")
+            logger.info("")
+        }
+
         logger.info("Loading config from: $configPath")
         val config = Configuration(configPath)
 
