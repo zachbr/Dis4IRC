37864551fec5feec2500977380cfb0b4e999e5b4
Zach Brown <zach@zachbr.io>
2025-12-21 11:16:05 -0600

    Update JDA

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ repositories {
 dependencies {
     implementation("org.kitteh.irc:client-lib:9.0.0")
     implementation("club.minnced:discord-webhooks:0.8.4")
-    implementation("net.dv8tion:JDA:6.1.3") {
+    implementation("net.dv8tion:JDA:6.2.0") {
         exclude(module = "opus-java")
         exclude(module = "tink")
     }
